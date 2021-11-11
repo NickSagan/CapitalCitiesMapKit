30fb41362fb535942b1643acb42fe6996f01bf14
Nikolai Saganenko <9952728@gmail.com>
2021-11-11 16:05:17 +0300

    finish Capital class

--- CapitalCitiesMapKit/Capital.swift
+++ CapitalCitiesMapKit/Capital.swift
@@ -10,4 +10,12 @@ import MapKit
 
 class Capital: NSObject, MKAnnotation {
     var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
 }
