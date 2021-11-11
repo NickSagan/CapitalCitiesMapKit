b29dad9f6e2064a4157b16a24121e3bd96fe9ad2
Nikolai Saganenko <9952728@gmail.com>
2021-11-11 15:57:05 +0300

    Initial Commit

---
+++ CapitalCitiesMapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapitalCitiesMapKit
+//
+//  Created by Nick Sagan on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
