fb49952c46944c46eb68784c216e6dc833a00871
Nikolai Saganenko <9952728@gmail.com>
2021-11-11 16:04:08 +0300

    add UI, MApKit

---
+++ CapitalCitiesMapKit/Capital.swift
@@ -0,0 +1,13 @@
+//
+//  Capital.swift
+//  CapitalCitiesMapKit
+//
+//  Created by Nick Sagan on 11.11.2021.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+}
--- CapitalCitiesMapKit/ViewController.swift
+++ CapitalCitiesMapKit/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
