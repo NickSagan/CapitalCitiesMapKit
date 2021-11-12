ffbc6d825a48a88270d0695891732ffc8161f5b3
Nikolai Saganenko <9952728@gmail.com>
2021-11-12 16:12:16 +0300

    Add map annotations

--- CapitalCitiesMapKit/ViewController.swift
+++ CapitalCitiesMapKit/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Is a capital of Great Britain")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Vikings lived here")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Beautiful city")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Pasta, Pizza!")
+        
+        mapView.addAnnotations([london, oslo, paris, rome])
     }
 
 
